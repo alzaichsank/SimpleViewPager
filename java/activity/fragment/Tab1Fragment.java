cdb1e80f125626465e874ad355fa34bf2e4ef999
Alza Devloper <alzaichsank@gmail.com>
2018-07-21 23:45:38 +0700

    AK: add fragment java

---
+++ java/activity/fragment/Tab1Fragment.java
@@ -0,0 +1,15 @@
+/*yourpackagename*/
+
+
+
+public class Tab1Fragment extends Fragment {
+   
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_tab1, container, false);
+
+        return rootView;
+    }
+
+}
---
+++ java/activity/fragment/Tab2Fragment.java
@@ -0,0 +1,15 @@
+/*yourpackagename*/
+
+
+
+public class Tab2Fragment extends Fragment {
+   
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_tab2, container, false);
+
+        return rootView;
+    }
+
+}
